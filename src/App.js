a4eeeab3ded4c3077f4bc9832b0b02377a863443
Awaiz <awaizkhann29@gmail.com>
2023-09-23 13:46:25 +0530

    Exploring Props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      id : 'E1',
+      title : "Toilet Paper",
+      amount : 94.12,
+      date : new Date(2020, 7, 14)
+    },
+    {
+      id : 'E2',
+      title : "Shrikhand",
+      amount : 84.12,
+      date : new Date(2020, 7, 15)
+    },
+    {
+      id : 'E3',
+      title : "Pasta",
+      amount : 100,
+      date : new Date(2020, 7, 18)
+    }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          helo nigga
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ExpenseItem title = {expenses[0].title} amount = {expenses[0].amount} date = {expenses[0].date}></ExpenseItem>
+      <ExpenseItem title = {expenses[1].title} amount = {expenses[1].amount} date = {expenses[1].date}></ExpenseItem>
+      <ExpenseItem title = {expenses[2].title} amount = {expenses[2].amount} date = {expenses[2].date}></ExpenseItem>
     </div>
   );
 }
